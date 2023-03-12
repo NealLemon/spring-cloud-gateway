db1e2aa1cc5bc1921771857c5be90b208756ee26
NealLemon <asas20@126.com>
2023-03-12 21:36:23 +0800

    update

---
+++ spring-cloud-gateway-server/src/main/java/org/springframework/cloud/gateway/route/AwesomeRouteLocator.java
@@ -0,0 +1,73 @@
+package org.springframework.cloud.gateway.route;
+
+import com.googlecode.cqengine.IndexedCollection;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.cloud.gateway.event.RefreshRoutesEvent;
+import org.springframework.cloud.gateway.event.RefreshRoutesResultEvent;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.AnnotationAwareOrderComparator;
+import reactor.core.publisher.Flux;
+
+import java.util.stream.Collectors;
+
+public class AwesomeRouteLocator implements Ordered, AwesomeRoutes, ApplicationListener<RefreshRoutesEvent>, ApplicationEventPublisherAware {
+
+	private static final Log log = LogFactory.getLog(AwesomeRouteLocator.class);
+
+	private final RouteLocator delegate;
+
+	private ApplicationEventPublisher applicationEventPublisher;
+
+	public AwesomeRouteLocator(RouteLocator delegate) {
+		this.delegate = delegate;
+	}
+
+	@Override
+	public Flux<Route> getRoutes() {
+		return null;
+	}
+
+	private Flux<Route> fetch() {
+		return this.delegate.getRoutes().sort(AnnotationAwareOrderComparator.INSTANCE);
+	}
+
+	@Override
+	public void onApplicationEvent(RefreshRoutesEvent event) {
+		try {
+			fetch().collect(Collectors.toList()).subscribe(
+					list -> Flux.fromIterable(list).materialize().collect(Collectors.toList()).subscribe(signals -> {
+						applicationEventPublisher.publishEvent(new RefreshRoutesResultEvent(this));
+						//TODO init QCengine
+					}, this::handleRefreshError), this::handleRefreshError);
+		}
+		catch (Throwable e) {
+			handleRefreshError(e);
+		}
+	}
+
+	private void handleRefreshError(Throwable throwable) {
+		if (log.isErrorEnabled()) {
+			log.error("Refresh routes error !!!", throwable);
+		}
+		applicationEventPublisher.publishEvent(new RefreshRoutesResultEvent(this, throwable));
+	}
+
+	@Override
+	public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+		this.applicationEventPublisher = applicationEventPublisher;
+	}
+
+	@Override
+	public int getOrder() {
+		return Ordered.HIGHEST_PRECEDENCE;
+	}
+
+	@Override
+	public IndexedCollection<Route> getCollectionRoutes() {
+		return null;
+	}
+}
---
+++ spring-cloud-gateway-server/src/main/java/org/springframework/cloud/gateway/route/WrapRoute.java
@@ -0,0 +1,18 @@
+package org.springframework.cloud.gateway.route;
+
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpMethod;
+
+//public class WrapRoute implements Ordered {
+//
+//	private final Route route;
+//
+//	private HttpMethod requestMethod;
+//
+//	private String
+//
+//	@Override
+//	public int getOrder() {
+//		return Ordered.HIGHEST_PRECEDENCE;
+//	}
+//}
